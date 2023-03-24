1ab29e93aa490d8290421fadd922a86e860f236a
unknown <glebegor2220@gmail.com>
2023-03-24 22:55:12 +0100

    created function run of server, initialization config, added config.

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	grod "github.com/Glebegor/Global-Repository-Of-Datasets/tree/master/back"
+	logrus "github.com/sirupsen/logrus"
+	viper "github.com/spf13/viper"
+)
 
+func main() {
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("Error initializating configs: %s", err.Error())
+	}
+	server := new(grod.Server)
+	if err := server.Run(viper.GetString("port")); err != nil {
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
