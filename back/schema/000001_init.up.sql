af5160754dc60a66390f2cdda009dab5855c6f04
unknown <glebegor2220@gmail.com>
2023-03-26 00:59:54 +0100

    created default field for up migrations

--- back/schema/000001_init.up.sql
+++ back/schema/000001_init.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE users (
     password_hash varchar(255) not null,
     email varchar(255) not null,
     tel varchar(255),
-    subscribe varchar(255) not null,
+    subscribe varchar(255) DEFAULT "free",
     id serial not null unique
 );
