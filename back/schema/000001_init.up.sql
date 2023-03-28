8a9c4ce44daccc0096d7762444ea2d0c41c5d0ff
unknown <glebegor2220@gmail.com>
2023-03-28 23:41:12 +0200

    created dataset table

--- back/schema/000001_init.up.sql
+++ back/schema/000001_init.up.sql
@@ -6,3 +6,8 @@ CREATE TABLE users (
     subscribe varchar(255) not null,
     id serial not null unique
 );
+CREATE TABLE datasets (
+    title varchar(255) not null,
+    description varchar(255) not null,
+    id serial not null unique,
+);
